d0170fe57d3906157fc65078a1dfc1dda2e4d3ff
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2016-09-12 11:10:25 +0200

    bhi

---
+++ SimonSaysUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _03_Simon_Says;
+
+namespace SimonSaysUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simon simon = new Simon();
+            string input = Console.ReadLine();
+            Console.WriteLine(simon.Echo(input));
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(simon.Shout(input));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
